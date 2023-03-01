c9ee422866450d1ab11c5374ef9b4d754c9e4871
eub1 <ebonforte@gmail.com>
2023-03-01 12:39:29 -0300

    refactor for mapping components

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -3,18 +3,42 @@ import { TwitterFollowCard } from './TwitterFollowCard';
 
 export default function App() {
 
+  const users= [
+    {
+      userName: 'midudev',
+      name: 'Miguel Angel Duran',
+      isFollowing: true
+    },
+    {
+      userName: 'eub1',
+      name: 'Eub1',
+      isFollowing: true
+    },
+    {
+      userName: 'pheralb_',
+      name: 'Pablo Heraldo',
+      isFollowing: false
+    },
+    {
+      userName: 'TMChein',
+      name: 'Tomas',
+      isFollowing: false
+    }
+  ]
+
   return (
     <div className='App'>
-      <TwitterFollowCard isFollowing>
-        eub1
-      </TwitterFollowCard>
-      <TwitterFollowCard isFollowing= {false} userName={"midudev"}>
-        Miguel Angel Duran
-      </TwitterFollowCard>
-      <TwitterFollowCard isFollowing= {false} userName={"pheralb_"}>
-        Pablo Hernandez
-      </TwitterFollowCard>
-
+      {
+        users.map(({ userName, name, isFollowing }) => (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+          ))
+      }
     </div>
   )
 }
