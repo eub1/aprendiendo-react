3b9d54953f61c1518f31c2d902b5d82ba0bdd024
eub1 <ebonforte@gmail.com>
2023-03-14 19:39:40 -0300

    responsive css

--- projects/05-react-buscador-peliculas/src/components/Movies.jsx
+++ projects/05-react-buscador-peliculas/src/components/Movies.jsx
@@ -1,8 +1,8 @@
 export function ListOfMovies({ movies }) {
   return (
-    <ul>
+    <ul className="movies">
       {movies.map((movie) => (
-        <li key={movie.id}>
+        <li className="movie" key={movie.id}>
           <h3>{movie.title}</h3>
           <p>{movie.year}</p>
           <img src={movie.poster} alt={movie.title} />
