f4bd832f271df7734b5787c5cde80e65d7e47a6c
eub1 <ebonforte@gmail.com>
2023-03-14 14:56:59 -0300

    create custom Hook useMovies

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,18 +1,9 @@
 import './App.css'
-import { Movies } from './components/Movies'
-import responseMovies from './mocks/with-results.json'
-// import withoutResults from './mocks/no-results.json'
+import { Movies } from './components/Movies.jsx'
+import { useMovies } from './hooks/useMovies.js'
 
 function App() {
-  // cuando hay peliculas? cuando tiene search y es un array (ver mock)
-  const movies = responseMovies.Search
-
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
+  const { movies: mappedMovies } = useMovies()
 
   return (
     <div className="page">
