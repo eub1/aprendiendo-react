262017a0bc40c84af9adafb17ab55d5dfeb601dc
eub1 <ebonforte@gmail.com>
2023-03-15 14:00:06 -0300

    add logic to prevent re-fetching same search

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import { searchMovies } from '../services/moviesService'
 
+let previousSerch = ''
+
 export function useMovies({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
   const getMovies = async () => {
+    if (search === previousSerch) return
+
     try {
       setLoading(true)
       setError(null)
+
+      previousSerch = search
 
       // recordar que searchMovies es asincrono
       const newMovies = await searchMovies({ search })
@@ -25,3 +31,8 @@ export function useMovies({ search }) {
 
   return { movies, getMovies, loading }
 }
+
+// uso la referencia, para verificar que no vuelva a llamar a la api
+// por la misma busqueda
+
+// entonces no hace nada, solo return
