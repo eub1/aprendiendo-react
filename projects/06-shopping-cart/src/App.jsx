a6a9f7ae80c288d93d89303fd8d7610b12fe6e3b
eub1 <ebonforte@gmail.com>
2023-03-20 19:56:56 -0300

    render Products

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,5 +1,8 @@
+import { products } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+
 function App() {
-  return <h1>Shopping Cart</h1>
+  return <Products products={products} />
 }
 
 export default App
