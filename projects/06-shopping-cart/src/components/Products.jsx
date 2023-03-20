8b8f62884a85d1eaba7918a7f8179d3e04ef6bc2
eub1 <ebonforte@gmail.com>
2023-03-20 19:56:47 -0300

    create Products component

---
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -0,0 +1,26 @@
+import './Products.css'
+import { AddToCartIcon } from './Icons.jsx'
+
+export function Products({ products }) {
+  return (
+    <main className="products">
+      <ul>
+        {products.slice(0, 10).map((product) => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+              <strong>{product.title}</strong> - ${product.price}
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+// products.slice(0, 10) para que no muestre todos
