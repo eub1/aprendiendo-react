0899d0c9ea60b0be90174c5cecefc0f0a13bf2f7
eub1 <ebonforte@gmail.com>
2023-03-28 18:55:14 -0300

    App.jsx return CartProvider

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,15 +3,16 @@ import { Products } from './components/Products.jsx'
 import { Footer } from './components/Footer.jsx'
 import { IS_DEVELOPMENT } from './config.js'
 import { Cart } from './components/Cart.jsx'
+import { CartProvider } from './context/cart.jsx'
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <Cart />
       <Products />
       {IS_DEVELOPMENT && <Footer />}
-    </>
+    </CartProvider>
   )
 }
 
