af15dd88b6e5d5124fed7154b5981a235be495a1
eub1 <ebonforte@gmail.com>
2023-03-30 19:57:57 -0300

    en comentarios alternativa refactor de reducer

--- projects/06-shopping-cart/src/reducers/cart.js
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -56,3 +56,48 @@ export const cartReducer = (state = cartInitialState, action) => {
   }
   return state
 }
+
+// alternativa refactor de reducer:
+/*
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTION_TYPES.ADD_TO_CART]: (state, action) => {
+    const { id } = actionPayload
+
+    const productInCartIndex = state.findIndex((item) => item.id === id)
+
+    if (productInCartIndex >= 0) {
+      const newState = structuredClone(state)
+      newState[productInCartIndex].quantity += 1
+      updateLocalStorage(newState)
+      return newState
+    }
+
+    const newState = [
+      ...state,
+      {
+        ...actionPayload, // product
+        quantity: 1
+      }
+    ]
+
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_ACTION_TYPES.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = actionPayload
+    const newState = state.filter((item) => item.id !== id)
+    updateLocalStorage(newState)
+    return newState
+  },
+  [CART_ACTION_TYPES.CLEAR_CART]: (state, action) => {
+    updateLocalStorage(cartInitialState)
+    return cartInitialState
+  }
+}
+
+export const cartReducer = (state, action) => {
+  const { type: actionType } = action
+  const updateState = UPDATE_STATE_BY_ACTION[actionType]
+  return updateState ? updateState(state, action) : state // es el default
+}
+ */
