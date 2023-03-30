bc700a8c3e6a450f332ea57f16eb1afd179d9bbb
eub1 <ebonforte@gmail.com>
2023-03-30 15:33:13 -0300

    comment footer JSON.stringify

--- projects/06-shopping-cart/src/components/Footer.jsx
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -1,17 +1,17 @@
-import { useCart } from '../hooks/useCart.js'
-import { useFilters } from '../hooks/useFilters.js'
+// import { useCart } from '../hooks/useCart.js'
+// import { useFilters } from '../hooks/useFilters.js'
 
 export function Footer() {
-  const { filters } = useFilters()
-  const { cart } = useCart()
+  // const { filters } = useFilters()
+  // const { cart } = useCart()
   return (
     <footer className="footer">
       <p>debugger for developer mode - change on production!!</p>
-      {JSON.stringify(filters, null, 2)}
-      {/* <h4>Prueba tecnica de React</h4>
+      {/* {JSON.stringify(filters, null, 2)} */}
+      <h4>Prueba tecnica de React</h4>
       <span>@midudev</span>
-      <h5>Shopping cart con useContext & useReducer</h5> */}
-      {JSON.stringify(cart, null, 2)}
+      <h5>Shopping cart con useContext & useReducer</h5>
+      {/* {JSON.stringify(cart, null, 2)} */}
     </footer>
   )
 }
